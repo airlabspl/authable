a8004b30503a0b456533aca5c557faf90dfa85a3
Krzysztof <krzysztof@engineer.com>
2017-11-01 18:38:14 +0100

    Checking permission via gate

--- src/AuthableServiceProvider.php
+++ src/AuthableServiceProvider.php
@@ -4,6 +4,7 @@ namespace Airlabs\Authable;
 
 use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AuthableServiceProvider extends ServiceProvider
@@ -16,9 +17,16 @@ class AuthableServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->database();
+        $this->routes();
 
         if (auth()->check() && $user = auth()->user()) {
+            $permissions = $user->role->permissions;
 
+            foreach ($permissions as $permission) {
+                Gate::define($permission->key, function () {
+                    return true;
+                });
+            }
         }
     }
 
@@ -26,6 +34,15 @@ class AuthableServiceProvider extends ServiceProvider
     {
         if (!$this->app->environment(['testing'])) {
             $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
+    }
+
+    protected function routes()
+    {
+        if ($this->app->environment('testing')) {
+            Route::get('air/auth/test', function () {
+                return 'Access';
+            })->middleware('can:air-test');
         }
     }
 }
--- tests/Feature/CheckingPermissionsTest.php
+++ tests/Feature/CheckingPermissionsTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Airlabs\Authable\Permission;
 use Airlabs\Authable\Role;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Tests\Fakes\Authenticable;
 use Tests\TestCase;
 
@@ -29,5 +31,35 @@ class CheckingPermissionsTest extends TestCase
         $user->assignRole($role);
 
         $this->assertTrue($user->hasPermission($permission));
+    }
+
+    /** @test */
+    function permissions_can_be_checked_via_authorization_gates()
+    {
+        $user = factory(Authenticable::class)->create();
+        $role = factory(Role::class)->create();
+        $permission = factory(Permission::class)->create([
+            'key' => 'air-test'
+        ]);
+        $role->permissions()->attach($permission);
+
+        $this->actingAs($user);
+
+        $response = $this->get('air/auth/test');
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $user->assignRole($role);
+
+        $permissions = $user->role->permissions;
+
+        foreach ($permissions as $permission) {
+            Gate::define($permission->key, function () {
+                return true;
+            });
+        }
+
+        $response = $this->get('air/auth/test');
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertSee('Access');
     }
 }
